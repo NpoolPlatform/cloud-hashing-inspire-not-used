454ad0f369fe57e26b9718311a4006ddfacc4125
Zhao KK <kikakkz@hotmail.com>
2021-11-08 08:51:22 +0000

    Add user invitation code

--- pkg/crud/user-invitation-code/userinvitationcode.go
+++ pkg/crud/user-invitation-code/userinvitationcode.go
@@ -1,1 +1,19 @@
 package userinvitationcode
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/cloud-hashing-inspire/message/npool"
+)
+
+func Create(ctx context.Context, in *npool.CreateUserInvitationCodeRequest) (*npool.CreateUserInvitationCodeResponse, error) {
+	return nil, nil
+}
+
+func Get(ctx context.Context, in *npool.GetUserInvitationCodeRequest) (*npool.GetUserInvitationCodeResponse, error) {
+	return nil, nil
+}
+
+func GetByAppUser(ctx context.Context, in *npool.GetUserInvitationCodeByAppUserRequest) (*npool.GetUserInvitationCodeByAppUserResponse, error) {
+	return nil, nil
+}
