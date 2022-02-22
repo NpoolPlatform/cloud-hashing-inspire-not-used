12de2b60ba36f81557ef34470c3602a683082dce
Zhao KK <kikakkz@hotmail.com>
2022-02-22 13:21:49 +0000

    Add event coupon

--- pkg/middleware/eventcoupon/eventcoupon.go
+++ pkg/middleware/eventcoupon/eventcoupon.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 
+	constant "github.com/NpoolPlatform/cloud-hashing-inspire/pkg/const"
 	activitycrud "github.com/NpoolPlatform/cloud-hashing-inspire/pkg/crud/activity"
 	eventcouponcrud "github.com/NpoolPlatform/cloud-hashing-inspire/pkg/crud/eventcoupon"
 	npool "github.com/NpoolPlatform/message/npool/cloud-hashing-inspire"
@@ -10,7 +11,26 @@ import (
 	"golang.org/x/xerrors"
 )
 
+func validateEvent(event string) error {
+	switch event {
+	case constant.EventSharing:
+	case constant.EventInvitationRegisteration:
+	case constant.EventInvitationPurchase:
+	case constant.EventRegister:
+	case constant.EventKYCAuthenticate:
+	case constant.EventTotalAmount:
+	case constant.EventSingleAmount:
+	default:
+		return xerrors.Errorf("invalid event")
+	}
+	return nil
+}
+
 func Create(ctx context.Context, in *npool.CreateEventCouponRequest) (*npool.CreateEventCouponResponse, error) {
+	if err := validateEvent(in.GetInfo().GetEvent()); err != nil {
+		return nil, xerrors.Errorf("invalid event: %v", err)
+	}
+
 	resp, err := activitycrud.Get(ctx, &npool.GetActivityRequest{
 		ID: in.GetInfo().GetActivityID(),
 	})
@@ -25,6 +45,10 @@ func Create(ctx context.Context, in *npool.CreateEventCouponRequest) (*npool.Cre
 }
 
 func Update(ctx context.Context, in *npool.UpdateEventCouponRequest) (*npool.UpdateEventCouponResponse, error) {
+	if err := validateEvent(in.GetInfo().GetEvent()); err != nil {
+		return nil, xerrors.Errorf("invalid event: %v", err)
+	}
+
 	resp, err := activitycrud.Get(ctx, &npool.GetActivityRequest{
 		ID: in.GetInfo().GetActivityID(),
 	})
