ec24bbf4bcd8ed0035a94f2bb19f03b8b0353ce2
Zhao KK <kikakkz@hotmail.com>
2022-02-22 07:46:12 +0000

    Add activity event

--- pkg/const/const.go
+++ pkg/const/const.go
@@ -3,4 +3,12 @@ package constant
 const (
 	CouponTypeCoupon   = "coupon"
 	CouponTypeDiscount = "discount"
+
+	EventSharing                 = "sharing"
+	EventInvitationRegisteration = "invitation-registeration"
+	EventInvitationPurchase      = "invitation-purchase"
+	EventRegister                = "register"
+	EventKYCAuthenticate         = "kyc-authenticate"
+	EventTotalAmount             = "total-amount"
+	EventSingleAmount            = "single-amount"
 )
